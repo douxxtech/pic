abdfc495027726e059eb304239eee79043918579
Douxx <douxx@douxx.tech>
2025-12-26 15:38:56 +0100

    installing node

--- pic.sh
+++ pic.sh
@@ -32,6 +32,9 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
+nvm install 25
+nvm use 25
+
 # install doual for alias managment
 npm i -g doual
 
